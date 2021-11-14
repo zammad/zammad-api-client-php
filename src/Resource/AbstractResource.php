ef46e05d40186faf03f4b17c100927ffe1cbdfa5
Mostafa Qanbaryan <mostafaqanbaryan@gmail.com>
2021-11-14 15:02:31 +0330

    Added: order_by in search function

--- src/Resource/AbstractResource.php
+++ src/Resource/AbstractResource.php
@@ -386,7 +386,7 @@ abstract class AbstractResource
      * @return mixed                        Returns array of ZammadAPIClient\Resource\... objects
      *                                          or this object on failure.
      */
-    public function search( $search_term, $page = null, $objects_per_page = null )
+    public function search( $search_term, $page = null, $objects_per_page = null, $sort_by = null, $order_by = null )
     {
         if ( !empty( $this->getValues() ) ) {
             throw new AlreadyFetchedObjectException('Object already contains values, search() not possible, use a new object');
@@ -406,7 +406,7 @@ abstract class AbstractResource
         }
 
         if ( !isset($page) || !isset($objects_per_page) ) {
-            return $this->searchWithoutPagination($search_term);
+            return $this->searchWithoutPagination($search_term, $sort_by, $order_by);
         }
 
         $url_parameters = [
@@ -417,6 +417,13 @@ abstract class AbstractResource
         if ( isset($page) && isset($objects_per_page) ) {
             $url_parameters['page']     = $page;
             $url_parameters['per_page'] = $objects_per_page;
+        }
+
+        if ( isset($sort_by) ) {
+            $url_parameters['sort_by'] = $sort_by;
+        }
+        if ( isset($order_by) ) {
+            $url_parameters['order_by'] = $order_by;
         }
 
         $url      = $this->getURL('search');
@@ -452,7 +459,7 @@ abstract class AbstractResource
      * @return mixed                        Returns array of ZammadAPIClient\Resource\... objects
      *                                          or this object on failure.
      */
-    private function searchWithoutPagination($search_term)
+    private function searchWithoutPagination($search_term, $sort_by = null, $order_by = null)
     {
         $page             = 1;
         $objects_per_page = 100;
@@ -460,7 +467,7 @@ abstract class AbstractResource
         $objects_of_page  = [];
 
         do {
-            $objects_of_page = $this->search( $search_term, $page, $objects_per_page );
+            $objects_of_page = $this->search( $search_term, $page, $objects_per_page, $sort_by, $order_by );
             if ( !is_array($objects_of_page) ) {
                 return $this;
             }
